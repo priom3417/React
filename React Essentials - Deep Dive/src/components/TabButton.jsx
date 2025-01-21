45ac00a6d3052e3c001a1429df66c3a587e41bce
Priom Bhowmik <priom417@gmail.com>
2025-01-21 13:14:53 +0600

    Delete React Essentials - Deep Dive directory

--- React Essentials - Deep Dive/src/components/TabButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function TabButton({ children, onSelect, isActiveTab }) {
-    return (
-      <li>
-        <button className={isActiveTab ? 'active' : ''} onClick={onSelect}>{children}</button>
-      </li>
-    );
-}
